218f6f426bcff6527dfd558517a532d0a0b6382a
gsh <gsh@XP1-B317-06-KT>
2013-01-18 11:36:20 +0100

    renvoi

--- TP-decouverte/src/Compte.java
+++ TP-decouverte/src/Compte.java
@@ -8,14 +8,45 @@ public class Compte {
 	
 	public Compte()
 	{
-		
 	}
-	
+
 	public Compte(String client, int numero, double solde)
 	{
 		this.client = client;
 		this.numero = numero ;
 		this.solde = solde ;
 	}
+
+	public String getClient() 
+	{
+		return client;
+	}
+
+	public void setClient(String client)
+	{
+		this.client = client;
+	}
+
+	public int getNumero() 
+	{
+		return numero;
+	}
+
+	public void setNumero(int numero)
+	{
+		this.numero = numero;
+	}
+
+	public double getSolde() 
+	{
+		return solde;
+	}
+
+	public void setSolde(double solde) 
+	{
+		this.solde = solde;
+	}
+	
+	
 
 }
